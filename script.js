e3b3bb2455ef068929c47f19d1734a633f483df1
mohdisasheikh <mdishk002@gmail.com>
2024-07-24 02:44:46 +0530

    Some files added

--- script.js
+++ script.js
@@ -32,122 +32,6 @@ function locomotive() {
 }
 locomotive();
 
-function menu() {
-  let mbtn = document.querySelector(".menubtn");
-  let toggle = true;
-
-  gsap.to(mbtn, {
-    scrollTrigger: {
-      scroller: ".wrapper",
-      trigger: ".sec4",
-      start: "top 50%",
-      scrub: true,
-    },
-    opacity: 1,
-    zIndex: "999",
-  });
-
-  mbtn.addEventListener("click", function () {
-    let tl2 = gsap.timeline();
-
-    if (toggle === true) {
-      gsap.to(".menubtn .textbox .open", {
-        y: "-100%",
-      });
-      gsap.to(".menubtn .textbox .close", {
-        y: "-100%",
-      });
-
-      tl2
-        .to(".l2", {
-          width: "0%",
-          duration: 0.3,
-        })
-        .to(
-          ".l1",
-          {
-            top: "50%",
-            duration: 0.3,
-          },
-          "efgh"
-        )
-        .to(
-          ".l3",
-          {
-            top: "50%",
-            duration: 0.3,
-          },
-          "efgh"
-        )
-        .to(
-          ".l1",
-          {
-            rotate: "45deg",
-            duration: 0.3,
-          },
-          "ijkl"
-        )
-        .to(
-          ".l3",
-          {
-            rotate: "-45deg",
-            duration: 0.3,
-          },
-          "ijkl"
-        );
-      toggle = false;
-    } else {
-      gsap.to(".menubtn .textbox .open", {
-        y: "initial",
-      });
-      gsap.to(".menubtn .textbox .close", {
-        y: "initial",
-      });
-
-      tl2
-        .to(
-          ".l1",
-          {
-            rotate: "0deg",
-            duration: 0.3,
-          },
-          "ijkl"
-        )
-        .to(
-          ".l3",
-          {
-            rotate: "0deg",
-            duration: 0.3,
-          },
-          "ijkl"
-        )
-        .to(
-          ".l1",
-          {
-            top: "5%",
-            duration: 0.3,
-          },
-          "efgh"
-        )
-        .to(
-          ".l3",
-          {
-            top: "95%",
-            duration: 0.3,
-          },
-          "efgh"
-        )
-        .to(".l2", {
-          width: "100%",
-          duration: 0.3,
-        });
-
-      toggle = true;
-    }
-  });
-}
-menu();
-
 function loader() {
   setTimeout(() => {
     const loader = document.querySelector(".loader");
@@ -166,6 +50,26 @@ function loader() {
 // loader();
 
 function sec1() {
+  const heading = document.querySelector(".sec1 h1");
+  const text = heading.textContent;
+  const wrappedText = text
+    .split("")
+    .map((letter) => `<span>${letter}</span>`)
+    .join("");
+  heading.innerHTML = wrappedText;
+
+  gsap.to(".sec1 h1 span", {
+    scrollTrigger: {
+      scroller: ".wrapper",
+      trigger: `.sec1`,
+      start: `top top`,
+      end: `+=250%`,
+      scrub: true,
+    },
+    color: "#fff",
+    stagger: 2,
+  });
+
   var tl1 = gsap.timeline({
     scrollTrigger: {
       scroller: ".wrapper",
@@ -192,7 +96,6 @@ function sec1() {
         ".sec1 h1",
         {
           top: "41%",
-          color: "#F8F8F8",
         },
         "abcd"
       );
@@ -211,7 +114,6 @@ function sec1() {
         ".sec1 h1",
         {
           top: "50%",
-          color: "#F8F8F8",
         },
         "abcd"
       );
@@ -242,3 +144,247 @@ function sec1() {
     });
 }
 sec1();
+
+function sec2() {
+  var sb = document.querySelector(".sec2 .sliderbox");
+  var mf = document.querySelector(".sec2 .sliderbox .clickFollower");
+  sb.addEventListener("mouseenter", function () {
+    gsap.to(mf, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  sb.addEventListener("mouseleave", function () {
+    gsap.to(mf, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+  sb.addEventListener("mousemove", function (dets) {
+    gsap.to(mf, {
+      left: dets.x - 760,
+      top: dets.y - 380,
+    });
+  });
+}
+// sec2();
+
+function sec3() {
+  const heading = document.querySelector(".sec3 h1");
+  const text = heading.textContent;
+  const wrappedText = text
+    .split("")
+    .map((letter) => `<span>${letter}</span>`)
+    .join("");
+  heading.innerHTML = wrappedText;
+
+  gsap.to(".sec3 h1 span", {
+    scrollTrigger: {
+      scroller: ".wrapper",
+      trigger: `.sec3`,
+      start: `top 30%`,
+      end: `top top`,
+      scrub: true,
+    },
+    color: "#fff",
+    stagger: 2,
+  });
+
+  // Canvas code ----->
+  const canvas = document.querySelector("canvas");
+  const context = canvas.getContext("2d");
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    render();
+  });
+
+  function files(index) {
+    var data = `
+    ./assets/canvas/Frames/scene00004.png
+    ./assets/canvas/Frames/scene00005.png
+    ./assets/canvas/Frames/scene00006.png
+    ./assets/canvas/Frames/scene00007.png
+    ./assets/canvas/Frames/scene00008.png
+    ./assets/canvas/Frames/scene00009.png
+    ./assets/canvas/Frames/scene00010.png
+    ./assets/canvas/Frames/scene00011.png
+    ./assets/canvas/Frames/scene00012.png
+    ./assets/canvas/Frames/scene00013.png
+    ./assets/canvas/Frames/scene00014.png
+    ./assets/canvas/Frames/scene00015.png
+    ./assets/canvas/Frames/scene00016.png
+    ./assets/canvas/Frames/scene00017.png
+    ./assets/canvas/Frames/scene00018.png
+    ./assets/canvas/Frames/scene00019.png
+    ./assets/canvas/Frames/scene00020.png
+    ./assets/canvas/Frames/scene00021.png
+    ./assets/canvas/Frames/scene00022.png
+    ./assets/canvas/Frames/scene00023.png
+    ./assets/canvas/Frames/scene00024.png
+    ./assets/canvas/Frames/scene00025.png
+    ./assets/canvas/Frames/scene00026.png
+    ./assets/canvas/Frames/scene00027.png
+    ./assets/canvas/Frames/scene00028.png
+    ./assets/canvas/Frames/scene00029.png
+    ./assets/canvas/Frames/scene00030.png
+    ./assets/canvas/Frames/scene00031.png
+    ./assets/canvas/Frames/scene00032.png
+    ./assets/canvas/Frames/scene00033.png
+    ./assets/canvas/Frames/scene00034.png
+    ./assets/canvas/Frames/scene00035.png
+    ./assets/canvas/Frames/scene00036.png
+    ./assets/canvas/Frames/scene00037.png
+    ./assets/canvas/Frames/scene00038.png
+    ./assets/canvas/Frames/scene00039.png
+    ./assets/canvas/Frames/scene00040.png
+    ./assets/canvas/Frames/scene00041.png
+    ./assets/canvas/Frames/scene00042.png
+    ./assets/canvas/Frames/scene00043.png
+    ./assets/canvas/Frames/scene00044.png
+    ./assets/canvas/Frames/scene00045.png
+    ./assets/canvas/Frames/scene00046.png
+    ./assets/canvas/Frames/scene00047.png
+    ./assets/canvas/Frames/scene00048.png
+    ./assets/canvas/Frames/scene00049.png
+    ./assets/canvas/Frames/scene00050.png
+    ./assets/canvas/Frames/scene00051.png
+    ./assets/canvas/Frames/scene00052.png
+    ./assets/canvas/Frames/scene00053.png
+    ./assets/canvas/Frames/scene00054.png
+    ./assets/canvas/Frames/scene00055.png
+    ./assets/canvas/Frames/scene00056.png
+    ./assets/canvas/Frames/scene00057.png
+    ./assets/canvas/Frames/scene00058.png
+    ./assets/canvas/Frames/scene00059.png
+    ./assets/canvas/Frames/scene00060.png
+    ./assets/canvas/Frames/scene00061.png
+    ./assets/canvas/Frames/scene00062.png
+    ./assets/canvas/Frames/scene00063.png
+    ./assets/canvas/Frames/scene00064.png
+    ./assets/canvas/Frames/scene00065.png
+    ./assets/canvas/Frames/scene00066.png
+    ./assets/canvas/Frames/scene00067.png
+    ./assets/canvas/Frames/scene00068.png
+    ./assets/canvas/Frames/scene00069.png
+    ./assets/canvas/Frames/scene00070.png
+    ./assets/canvas/Frames/scene00071.png
+    ./assets/canvas/Frames/scene00072.png
+    ./assets/canvas/Frames/scene00073.png
+    `;
+    return data.split("\n")[index];
+  }
+
+  const frameCount = 70;
+
+  const images = [];
+  const imageSeq = {
+    frame: 1,
+  };
+
+  for (let i = 0; i < frameCount; i++) {
+    const img = new Image();
+    img.src = files(i);
+    images.push(img);
+  }
+
+  gsap.to(imageSeq, {
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: `none`,
+    scrollTrigger: {
+      scrub: 0.15,
+      trigger: `.sec3>canvas`,
+      start: `top top`,
+      end: `400% top`,
+      scroller: ".wrapper",
+    },
+    onUpdate: render,
+  });
+
+  images[1].onload = render;
+
+  function render() {
+    scaleImage(images[imageSeq.frame], context);
+  }
+
+  function scaleImage(img, ctx) {
+    var canvas = ctx.canvas;
+    var hRatio = canvas.width / img.width;
+    var vRatio = canvas.height / img.height;
+    var ratio = Math.max(hRatio, vRatio);
+    var centerShift_x = (canvas.width - img.width * ratio) / 2;
+    var centerShift_y = (canvas.height - img.height * ratio) / 2;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(
+      img,
+      0,
+      0,
+      img.width,
+      img.height,
+      centerShift_x,
+      centerShift_y,
+      img.width * ratio,
+      img.height * ratio
+    );
+  }
+
+  ScrollTrigger.create({
+    trigger: ".sec3>canvas",
+    pin: true,
+    start: `top top`,
+    end: `400% top`,
+    scroller: ".wrapper",
+  });
+}
+
+sec3();
+
+function sec4() {
+  gsap.to(".st", {
+    repeat: -1,
+    x: "-100%",
+    ease: "linear",
+    duration: 6,
+  });
+}
+sec4();
+
+function sec5() {
+  var tl2 = gsap.timeline({
+    scrollTrigger: {
+      scroller: ".wrapper",
+      trigger: ".sec5",
+      start: "top top",
+      pin: true,
+      end: "+=250%",
+      scrub: true,
+    },
+  });
+
+  tl2
+    .to(
+      ".reel",
+      {
+        scale: 1,
+      },
+      "jklm"
+    )
+    .to(
+      ".reeltext",
+      {
+        wordSpacing: "0vmax",
+      },
+      "jklm"
+    );
+}
+sec5();
+
+function mouseEffect() {
+  Shery.makeMagnet(".magnet");
+}
+mouseEffect();
